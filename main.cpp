c37d53e5d48413f87bfb82351bd07a73818902c2
TuyenTuyenTuyen <1975vuong@gmail.com>
2025-10-31 13:19:46 +0700

    - Thêm các bộ phận của Rắn, cơ chế di chuyển giống rắn - Thay đổi thứ tự vẽ nền và vật thể đè lên nó, điều này giúp trành nền đè lên các vật thể khi vẽ sau - Thêm class SnakeSegments - Thay đổi kiểu của biến direction, và các điều kiện để phù hợp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
+class SnakeSegment {
+    private:
+        sf::RectangleShape shape;
+    public:
+        SnakeSegment() : shape({40.f, 40.f}) {
+            shape.setFillColor(sf::Color::Blue);
+            shape.setOrigin(shape.getLocalBounds().getCenter());
+            shape.setOutlineThickness(1.f);
+            shape.setOutlineColor(sf::Color::Black);
+        }
+
+        void setPosition(sf::Vector2f position) {
+            shape.setPosition(position);
+        }
+
+        void draw(sf::RenderWindow &window) {
+            window.draw(shape);
+        }
+
+        sf::Vector2f getPosition() {
+            return shape.getPosition();
+        }
+};
 
 int main() {
-    unsigned int maxXCells = 40, maxYCells = 20;
+    unsigned int maxXCells = 40, maxYCells = 20, segmentsNumber = 10;
 
     sf::RenderWindow window(sf::VideoMode({maxXCells * 40, maxYCells * 40}), "Snake Game");
     window.setFramerateLimit(10);
 
     float speed = 40.f;
     float distance;
-    int direction = 0;
+    char direction = 'O';
 
-    sf::CircleShape circle(20.f);
-    circle.setFillColor(sf::Color::Blue);
-    circle.setPosition({20.f, 20.f});
-    circle.setOrigin({20.f, 20.f});
+    sf::CircleShape snakeHead(20.f);
+    snakeHead.setFillColor(sf::Color::Red);
+    snakeHead.setPosition({20.f, 20.f});
+    snakeHead.setOrigin(snakeHead.getLocalBounds().getCenter());
+    snakeHead.setOutlineThickness(1.f);
+    snakeHead.setOutlineColor(sf::Color::Black);
+
+    std::vector<SnakeSegment> snakeSegmentVector;
+    for (int index = 0; index < segmentsNumber; index++) {
+        snakeSegmentVector.emplace_back();
+    }
+    SnakeSegment &firstSegment = snakeSegmentVector.at(0);
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -21,35 +54,19 @@ int main() {
                 window.close();
             else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
                 if (keyPressed->scancode == sf::Keyboard::Scancode::W)
-                    direction = 1;
+                    direction = 'W';
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::A)
-                    direction = 2;
+                    direction = 'A';
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::S)
-                    direction = 3;
+                    direction = 'S';
                 else if (keyPressed->scancode == sf::Keyboard::Scancode::D)
-                    direction = 4;
+                    direction = 'D';
             }
-        }
-
-        distance = speed;
-        
-        switch (direction) {
-            case 1:
-                circle.move({0.f, -distance}); // Lên
-                break;
-            case 2:
-                circle.move({-distance, 0.f}); // Trái
-                break;
-            case 3:
-                circle.move({0.f, distance}); // Xuống
-                break;
-            case 4:
-                circle.move({distance, 0.f}); // Phải
-                break;
         }
 
         window.clear();
 
+        //  Hình nền
         for (int y = 0; y < maxYCells; y++) {
             for (int x = 0; x < maxXCells; x++) {
                 sf::RectangleShape cell({40.f, 40.f});
@@ -62,9 +79,35 @@ int main() {
             }
         }
 
-        window.draw(circle);
+        for (int index = segmentsNumber - 1; index > 0; index--) {
+            SnakeSegment &segment = snakeSegmentVector.at(index);
+            segment.setPosition(snakeSegmentVector.at(index - 1).getPosition());
+            segment.draw(window);
+        }
+
+        firstSegment.setPosition(snakeHead.getPosition());
+        firstSegment.draw(window);
+
+        distance = speed;
+        
+        switch (direction) {
+            case 'W':
+                snakeHead.move({0.f, -distance}); // Lên
+                break;
+            case 'A':
+                snakeHead.move({-distance, 0.f}); // Trái
+                break;
+            case 'S':
+                snakeHead.move({0.f, distance}); // Xuống
+                break;
+            case 'D':
+                snakeHead.move({distance, 0.f}); // Phải
+                break;
+        }
+
+        window.draw(snakeHead);
+
         window.display();
     }
-
     return 0;
 }
