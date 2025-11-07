d845788259d6713389ef625f474089a83b19d81c
TuyenTuyenTuyen <1975vuong@gmail.com>
2025-11-07 20:51:32 +0700

    - Đồ ăn của rắn không đè lên thân rắn sau khi ăn - Thêm mảng động 2 chiều visitedGrid, kiểm tra các vị trí thân rắn và đầu rắn đã chiếm - Thay thân rắn từ hình vuông sang hình tròn - Sữa lỗi khi người chơi nhập phím di chuyển nhanh, tức có nhiều sự kiện nhập phím, dẫn đến rắn tự cắn chính mình - Dùng .reserve cho snakeSegmentsVector để tăng hiệu năng - Thêm một phần thân rắn khi rắn cắn trúng thân, điều nay để đảm bảo độ dài của rắn vì ta không cập nhật vị trí của đầu rắn sau khi tự cắn - Cho phép rắn đi xuyên các cạnh của cửa sổ game
    VẤN ĐỀ ĐANG GẶP PHẢI:
    - Nếu độ dài ban đầu của rắn dài quá dài, khiến ít nhất một phần tử tràn ra màn hình, sẽ gây ra lỗi truy cập mảng (vì vị trí lúc đó là âm, dẫn đến ta đang truy cập tại chỉ số âm)
    - Khi bắt đầu game, đồ ăn vẫn có thể xuất hiện trên thân rắn

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 class SnakeSegment {
     private:
-        sf::RectangleShape shape;
+        sf::CircleShape shape;
     public:
-        SnakeSegment(sf::Vector2f initialPosition) : shape({40.f, 40.f}) {
+        SnakeSegment(sf::Vector2f initialPosition) : shape(18.f) {
             shape.setFillColor(sf::Color::Blue);
             shape.setOrigin(shape.getLocalBounds().getCenter());
             shape.setOutlineThickness(1.f);
@@ -31,8 +31,8 @@ class Food {
     private:
         sf::CircleShape shape;
     public:
-        Food(sf::Vector2f initialPosition) : shape({20.f}) {
-            shape.setFillColor(sf::Color::Red);
+        Food(sf::Vector2f initialPosition) : shape({15.f}) {
+            shape.setFillColor(sf::Color::Magenta);
             shape.setOrigin(shape.getLocalBounds().getCenter());
             shape.setPosition(initialPosition);
         }
@@ -52,27 +52,31 @@ class Food {
 
 int main() {
     // Những giá trị khởi đầu
-    unsigned int maxXCells = 10, maxYCells = 10, initialSegmentNumber = 3;
+    unsigned int maxXCells = 10, maxYCells = 10, initialSegmentNumber = 5;
     float speed = 40.f, distance;
     bool isDied = false;
     char direction = 'O';
+    char currentDirection = 'O';
 
     std::random_device random;
     std::mt19937 gen(random());
     std::uniform_int_distribution<int> distX(0, maxXCells - 1);
     std::uniform_int_distribution<int> distY(0, maxYCells - 1);
 
+    std::vector<std::vector<int>> visitedGrid(maxYCells, std::vector<int>(maxXCells, 0));
+
     sf::RenderWindow window(sf::VideoMode({maxXCells * 40, maxYCells * 40}), "Snake Game");
     window.setFramerateLimit(10);
 
     sf::CircleShape snakeHead(20.f);
     snakeHead.setFillColor(sf::Color::Red);
-    snakeHead.setPosition({maxXCells / 2.f * 40.f + 20.f, maxYCells / 2.f * 40.f + 20.f});
+    snakeHead.setPosition({maxXCells / 2 * 40.f + 20.f, maxYCells / 2 * 40.f + 20.f});
     snakeHead.setOrigin(snakeHead.getLocalBounds().getCenter());
     snakeHead.setOutlineThickness(1.f);
     snakeHead.setOutlineColor(sf::Color::Black);
 
     std::vector<SnakeSegment> snakeSegmentVector;
+    snakeSegmentVector.reserve(maxXCells * maxYCells);
     for (int index = 0; index < initialSegmentNumber; index++) {
         sf::Vector2f initalSegmentPosition({snakeHead.getPosition().x - index * 40.f - 40.f, snakeHead.getPosition().y});
         snakeSegmentVector.emplace_back(initalSegmentPosition);
@@ -96,6 +100,12 @@ int main() {
             }
         }
 
+        if ((currentDirection == 'W' && direction == 'S') || 
+            (currentDirection == 'A' && direction == 'D') || 
+            (currentDirection == 'S' && direction == 'W') || 
+            (currentDirection == 'D' && direction == 'A'))
+            direction = currentDirection;
+
         window.clear();
 
         //  Hình nền
@@ -111,43 +121,64 @@ int main() {
             }
         }
 
-        if (isDied == false) {
+        if (!isDied && direction != 'O') {
             if (snakeHead.getPosition() == snakeFood.getPosition()) {
-                snakeFood.setPosition({static_cast<float>(distX(gen)) * 40.f + 20.f, static_cast<float>(distY(gen)) * 40.f + 20.f});
                 snakeSegmentVector.emplace_back(snakeSegmentVector.back().getPosition());
+                do snakeFood.setPosition({static_cast<float>(distX(gen)) * 40.f + 20.f, static_cast<float>(distY(gen)) * 40.f + 20.f});
+                while (visitedGrid.at(static_cast<int>(snakeFood.getPosition().y) / 40).at(static_cast<int>(snakeFood.getPosition().x) / 40) == 1);
             }
 
-            if (direction != 'O') {
-                for (int index = static_cast<int>(snakeSegmentVector.size()) - 1; index > 0; index--) {
-                        snakeSegmentVector.at(index).setPosition(snakeSegmentVector.at(index - 1).getPosition());
+            for (int index = 0; index < maxYCells; index++)
+                std::fill(visitedGrid.at(index).begin(), visitedGrid.at(index).end(), 0);
 
-                        if (snakeHead.getPosition() == snakeSegmentVector.at(index).getPosition()) 
-                            isDied = true;
-                }
+            sf::Vector2f tailPosition = snakeSegmentVector.back().getPosition();
+            for (int index = static_cast<int>(snakeSegmentVector.size()) - 1; index > 0; index--) {
+                snakeSegmentVector.at(index).setPosition(snakeSegmentVector.at(index - 1).getPosition());
 
-                snakeSegmentVector.at(0).setPosition(snakeHead.getPosition());
-            
-                if (isDied)
-                    continue;
-            
-                switch (direction) {
-                    case 'W':
-                        snakeHead.move({0.f, -speed}); // Lên
-                        break;
-                    case 'A':
-                        snakeHead.move({-speed, 0.f}); // Trái
-                        break;
-                    case 'S':
-                        snakeHead.move({0.f, speed}); // Xuống
-                        break;
-                    case 'D':
-                        snakeHead.move({speed, 0.f}); // Phải
-                        break;
+                if (snakeHead.getPosition() == snakeSegmentVector.at(index).getPosition()) {
+                    isDied = true;
+                    // Thêm một phần tử mới để tránh bù lại bộ phận bị khi rắn cắn trúng thân, do ta không cập nhật vị trí đầu rắn sau này
+                    snakeSegmentVector.emplace_back(tailPosition);
                 }
             }
+
+            snakeSegmentVector.at(0).setPosition(snakeHead.getPosition());
+        
+            if (isDied)
+                continue;
+        
+            switch (direction) {
+                case 'W':
+                    snakeHead.move({0.f, -speed}); // Lên
+                    break;
+                case 'A':
+                    snakeHead.move({-speed, 0.f}); // Trái
+                    break;
+                case 'S':
+                    snakeHead.move({0.f, speed}); // Xuống
+                    break;
+                case 'D':
+                    snakeHead.move({speed, 0.f}); // Phải
+                    break;
+            }
+            currentDirection = direction;
+
+            // Cho phép đi xuyên không gian
+            if (snakeHead.getPosition().x < 0.f)
+                snakeHead.setPosition({maxXCells * 40.f - 20.f, snakeHead.getPosition().y});
+            else if (snakeHead.getPosition().x > maxXCells * 40.f)
+                snakeHead.setPosition({20.f, snakeHead.getPosition().y});
+            else if (snakeHead.getPosition().y < 0.f)
+                snakeHead.setPosition({snakeHead.getPosition().x, maxYCells * 40.f - 20.f});
+            else if (snakeHead.getPosition().y > maxYCells * 40.f)
+                snakeHead.setPosition({snakeHead.getPosition().x, 20.f});
+
+            for (int index = 0; index < static_cast<int>(snakeSegmentVector.size()); index++)
+                 visitedGrid.at(static_cast<int>(snakeSegmentVector.at(index).getPosition().y) / 40).at(static_cast<int>(snakeSegmentVector.at(index).getPosition().x) / 40) = 1;
+            visitedGrid.at(static_cast<int>(snakeHead.getPosition().y) / 40).at(static_cast<int>(snakeHead.getPosition().x) / 40) = 1;
         }
 
-        for (int index = static_cast<int>(snakeSegmentVector.size()) - 1; index >= 0; index--)
+        for (int index = 0; index < static_cast<int>(snakeSegmentVector.size()); index++)
             snakeSegmentVector.at(index).draw(window);
 
         window.draw(snakeHead);
